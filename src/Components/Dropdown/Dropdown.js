d6317b4a02cb0889b886deeac571a5ffd268bfe2
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-17 10:52:54 -0700

    added styling and a placeholder to Dropdown

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,5 +1,16 @@
 const Dropdown = (props) => {
-  return <div>{props.children}</div>;
+  const { coords } = props;
+  const style = {
+    position: "absolute",
+    left: coords.x - 25,
+    top: coords.y + 50,
+  };
+  return (
+    <div style={style}>
+      {props.children}
+      <button>Placeholder</button>
+    </div>
+  );
 };
 
 export default Dropdown;
