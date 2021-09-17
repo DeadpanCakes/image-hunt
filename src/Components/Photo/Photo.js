76fdc5513108c60f563d0d394df463366272f35d
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-16 19:16:16 -0700

    added props and state to Photo to resize based on imgSrc and move target based on click events

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,9 +1,32 @@
 import "./Photo.css";
+import { useState } from "react";
+import Target from "../Target/Target";
 
 const Photo = (props) => {
-  const { imgSrc } = props;
-  const photoStyle = { backgroundImage: `url(${imgSrc})`};
-  return <div className="photo" style={photoStyle}></div>;
+  const { imgSrc, imgWidth, imgHeight } = props;
+  const [targetPosition, setTargetPosition] = useState(null);
+
+  const photoStyle = {
+    backgroundImage: `url(${imgSrc})`,
+    width: imgWidth,
+    height: imgHeight,
+  };
+
+  const handleClick = (coords) => {
+    setTargetPosition(coords);
+  };
+
+  return (
+    <div
+      className="photo"
+      style={photoStyle}
+      onClick={(e) => {
+        handleClick({ x: e.pageX, y: e.pageY });
+      }}
+    >
+      {targetPosition ? <Target coords={targetPosition} /> : null}
+    </div>
+  );
 };
 
 export default Photo;
