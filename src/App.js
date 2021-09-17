9e20771ec2db1fc64bbb6604961df1ec49fbe5d0
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-17 16:56:24 -0700

    made basic skeleton of app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import Photo from "./Components/Photo/Photo";
+import targetPool from "./testFns/tmpTarget/leftTmpTarget";
+import panelImgs from "./Assets/panels";
+import Game from "./Components/Game/Game";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {
+        <Game>
+          <Header />
+          <Photo
+            imgSrc={panelImgs.left}
+            imgWidth={1800}
+            imgHeight={4300}
+            targetPool={targetPool}
+          />
+        </Game>
+      }
+    </div>
+  );
 }
 
 export default App;
