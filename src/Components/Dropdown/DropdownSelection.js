b4f691c4ff6bce1d772b81a06762f4ce69ab7748
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-17 12:40:18 -0700

    added functionality to check whether passed coords mark the target

--- src/Components/Dropdown/DropdownSelection.js
+++ src/Components/Dropdown/DropdownSelection.js
@@ -1,10 +1,41 @@
 const DropdownSelection = (props) => {
-  const { target } = props;
-  return (
-    <div>
-      <p>{target.name}</p>
-    </div>
-  );
+  const { target, targetBox } = props;
+  const { position } = target;
+  const checkIfTargeted = (coords) => {
+    const topLeft = {
+      x: position.origin.x - position.size / 2,
+      y: position.origin.y + position.size / 2,
+    };
+    const bottomRight = {
+      x: position.origin.x + position.size / 2,
+      y: position.origin.y - position.size / 2,
+    };
+    const isWithinX = coords.x > topLeft.x && coords.x < bottomRight.x;
+    const isWithinY = coords.y < topLeft.y && coords.y > bottomRight.y;
+    return isWithinX && isWithinY;
+  };
+
+  const doBoxesOverlap = (origin, size) => {
+    const topLeft = { x: origin.x - size / 2, y: origin.y + size / 2 };
+    const topRight = { x: origin.x + size / 2, y: origin.y + size / 2 };
+    const bottomRight = {
+      x: origin.x + size / 2,
+      y: origin.y - size / 2,
+    };
+    const bottomLeft = { x: origin.x - size / 2, y: origin.y - size / 2 };
+    return (
+      checkIfTargeted(topLeft) ||
+      checkIfTargeted(topRight) ||
+      checkIfTargeted(bottomRight) ||
+      checkIfTargeted(bottomLeft) ||
+      checkIfTargeted(origin)
+    );
+  };
+
+  const handleClick = () => {
+    console.log(doBoxesOverlap(targetBox.origin, targetBox.size));
+  };
+  return <button onClick={handleClick}>{target.desc}</button>;
 };
 
 export default DropdownSelection;
