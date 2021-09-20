689c5e4e1fa58440774d0b6d4517f9604d5ba5c4
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-20 16:37:38 -0700

    made handleClick fn to track what level user wants to play

--- src/Components/LevelSelect/LevelSelect.js
+++ src/Components/LevelSelect/LevelSelect.js
@@ -1,13 +1,33 @@
 import "./LevelSelect.css";
 import gardenImg from "../../Assets/gardenImg.png";
 
-const LevelSelect = () => {
+const LevelSelect = (props) => {
+  const { choosePanel } = props;
   const style = {
     backgroundImage: `url(${gardenImg})`,
     minWidth: 1587,
     minHeight: 893,
   };
-  return <div className="levelSelect" style={style}></div>;
+  const handleClick = (xValue) => {
+    let panel;
+    if (xValue < 316) {
+      panel = "left";
+    } else if (xValue > 1112) {
+      panel = "right";
+    } else {
+      panel = "center";
+    }
+    choosePanel(panel);
+  };
+  return (
+    <div
+      className="levelSelect"
+      style={style}
+      onClick={(e) => {
+        handleClick(e.pageX);
+      }}
+    ></div>
+  );
 };
 
 export default LevelSelect;
