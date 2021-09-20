5712600dfd67b06d52e495aea9d672c95923e92a
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-20 16:26:43 -0700

    styled MainMenu  and added LevelSelect

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -1,5 +1,13 @@
-const MainMenu = () => {
-  return <div className="mainMenu"></div>;
+import "./MainMenu.css";
+import LevelSelect from "../LevelSelect/LevelSelect";
+
+const MainMenu = (props) => {
+  return (
+    <div className="mainMenu">
+      <h1>Garden of Earthly Delights</h1>
+      <LevelSelect choosePanel={props.choosePanel} />
+    </div>
+  );
 };
 
 export default MainMenu;
