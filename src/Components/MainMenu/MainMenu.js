f58e67db5d50acfad50dc5b7b8e78378f6c43142
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-21 13:16:49 -0700

    Added font to MainMenu and adjusted h1

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -4,7 +4,7 @@ import LevelSelect from "../LevelSelect/LevelSelect";
 const MainMenu = () => {
   return (
     <div className="mainMenu">
-      <h1>Garden of Earthly Delights</h1>
+      <h1>The Garden of Earthly Delights</h1>
       <LevelSelect />
     </div>
   );
