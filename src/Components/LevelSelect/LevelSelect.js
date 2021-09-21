4a64e1ccde8d1e2a0a92b1623db8ef0dc2bbe301
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-21 12:41:38 -0700

    added Links to LevelSelect and styled

--- src/Components/LevelSelect/LevelSelect.js
+++ src/Components/LevelSelect/LevelSelect.js
@@ -1,32 +1,32 @@
 import "./LevelSelect.css";
 import gardenImg from "../../Assets/gardenImg.png";
+import { Link } from "react-router-dom";
 
-const LevelSelect = (props) => {
-  const { choosePanel } = props;
+const LevelSelect = () => {
   const style = {
     backgroundImage: `url(${gardenImg})`,
     minWidth: 1587,
     minHeight: 893,
   };
-  const handleClick = (xValue) => {
-    let panel;
-    if (xValue < 316) {
-      panel = "left";
-    } else if (xValue > 1112) {
-      panel = "right";
-    } else {
-      panel = "center";
-    }
-    choosePanel(panel);
-  };
+
   return (
-    <div
-      className="levelSelect"
-      style={style}
-      onClick={(e) => {
-        handleClick(e.pageX);
-      }}
-    ></div>
+    <div className="levelSelect" style={style}>
+      <Link to="/game/left" className="leftPanel levelOption">
+        <div>
+          <h2>Left</h2>
+        </div>
+      </Link>
+      <Link to="/game/center" className="centerPanel levelOption">
+        <div>
+          <h2>Center</h2>
+        </div>
+      </Link>
+      <Link to="/game/right" className="rightPanel levelOption">
+        <div>
+          <h2>Right</h2>
+        </div>
+      </Link>
+    </div>
   );
 };
 
