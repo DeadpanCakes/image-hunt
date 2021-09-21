93309a65fb770ae9a73e0f114fb10cf4d7680238
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-21 16:23:22 -0700

    added extra styling to position pins correctly

--- src/Components/Pin/Pin.js
+++ src/Components/Pin/Pin.js
@@ -3,11 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapPin } from "@fortawesome/free-solid-svg-icons";
 
 const Pin = (props) => {
-  const { name, position } = props;
+  const { name, position, isCorrect } = props;
   return (
-    <div className="pinContainer" style={{ left: position.x, top: position.y }}>
-      <FontAwesomeIcon icon={faMapPin} className="pin" />
-      <p>{name}</p>
+    <div
+      className="pinContainer"
+      style={{
+        left: position.x - 5,
+        top: position.y - 25,
+        color: isCorrect ? "white" : "red",
+      }}
+    >
+      <div className="iconContainer">
+        <FontAwesomeIcon icon={faMapPin} className="pin" />
+        <p
+          className="name"
+          style={{ textDecoration: isCorrect ? "none" : "line-through" }}
+        >
+          {name}
+        </p>
+      </div>
     </div>
   );
 };
