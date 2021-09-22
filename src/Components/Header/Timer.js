505108ea0a20b61e5361d2c5c14b40795cfa7536
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-09-21 17:05:28 -0700

    added functionality to Timer

--- src/Components/Header/Timer.js
+++ src/Components/Header/Timer.js
@@ -1,4 +1,36 @@
+import { useState, useEffect } from "react";
+
 const Timer = () => {
-  return <h3 className="timer">00:00:00</h3>;
+  const [currentTime, setCurrentTime] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentTime((currTime) => currTime + 1);
+    }, 1000);
+  }, []);
+  const convertTime = (seconds) => {
+    let hours;
+    let minutes;
+    if (seconds >= 60) {
+      minutes = Math.floor(seconds / 60);
+      if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+    } else {
+      minutes = "00";
+    }
+    if (minutes >= 60) {
+      hours = Math.floor(minutes / 60);
+      if (hours < 10) {
+        minutes = `0${hours}`;
+      }
+    } else {
+      hours = "00";
+    }
+    return `${hours}: ${minutes}: ${
+      seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60
+    }`;
+  };
+
+  return <h3 className="timer">{convertTime(currentTime)}</h3>;
 };
 export default Timer;
